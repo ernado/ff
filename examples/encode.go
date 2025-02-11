c9e966bee9656188e055394cb6e56c95d27166cd
Aleksandr Razumov <ernado@go-faster.org>
2025-02-11 13:44:14 +0300

    test: add example

--- examples/encode.go
+++ examples/encode.go
@@ -1,11 +1,3 @@
-# ff [![Go Reference](https://img.shields.io/badge/go-pkg-00ADD8)](https://pkg.go.dev/github.com/ernado/ff#section-documentation) [![codecov](https://img.shields.io/codecov/c/github/ernado/ff?label=cover)](https://codecov.io/gh/ernado/ff) [![experimental](https://img.shields.io/badge/-experimental-blueviolet)](https://ernado.org/docs/projects/status#experimental)
-
-
-Helpers for running ffmpeg commands in Go.
-
-## Example
-
-```go
 package main
 
 import (
@@ -72,5 +64,3 @@ func main() {
 		os.Exit(1)
 	}
 }
-
-```
